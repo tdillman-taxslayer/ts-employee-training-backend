5eb4ccf486d9e453089645acc7f1ed329f84f660
RudsonA <rudson.augustin@yahoo.com>
2019-05-21 09:18:46 -0400

    beforesave trigger done

--- dist/cloud/beforeSave.js
+++ dist/cloud/beforeSave.js
@@ -1,6 +1,5 @@
-"use strict";var _https = require("https");
-
-Parse.Cloud.beforeSave(Parse.User, function (request, response) {
+"use strict";
+Parse.Cloud.beforeSave(Parse.User, function (request) {
     var emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
     var usernameRegex = /^[a-z\d]{5,12}$/i;
     var passwordRegex = /^[\w@-]{8,20}$/;
@@ -8,30 +7,20 @@ Parse.Cloud.beforeSave(Parse.User, function (request, response) {
     var user = request.object;
 
     if (emailRegex.test(user.get("email"))) {
-        // response.success();
         console.log('Success');
-
     } else {
-        response.error("Email must be a valid address, e.g. me@mydomain.com");
-        // throw "Email must be a valid address, e.g. me@mydomain.com";
+        throw "Email must be a valid address, e.g. me@mydomain.com";
     }
 
     if (usernameRegex.test(user.get("username"))) {
-        // response.success();
         console.log('Success');
-
     } else {
-        // response.error("Username must be  and contain 5 - 12 characters.");
         throw "Username must be  and contain 5 - 12 characters.";
     }
 
     if (passwordRegex.test(user.get("password"))) {
-        // response.success();
         console.log('Success');
-
     } else {
-        // response.error("Password must alphanumeric (@, _ and - are also allowed) and be 8 - 20 characters. ");
         throw "Password must alphanumeric (@, _ and - are also allowed) and be 8 - 20 characters. ";
     }
-
 });
