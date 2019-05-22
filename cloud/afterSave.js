e0b68b5079136987a31e7a3a02f60ca9e7e76848
RudsonA <rudson.augustin@yahoo.com>
2019-05-22 09:27:02 -0400

    aftersave user -> employee, user -> role

--- cloud/afterSave.js
+++ cloud/afterSave.js
@@ -2,10 +2,24 @@ Parse.Cloud.afterSave(Parse.User, async req => {
     // Init new employee 
     const query = new Parse.Query("Employee");
     query.equalTo("Owner", req.object);
-    let u = await query.first({useMasterKey: true});
+    let u = await query.first({
+        useMasterKey: true
+    });
     if (!u) {
-    let newEmployee = new Parse.Object("Employee"); 
-    newEmployee.set("Owner", req.object); 
-    await newEmployee.save({}, {useMasterKey: true});
+        let newEmployee = new Parse.Object("Employee");
+        newEmployee.set("Owner", req.object);
+        await newEmployee.save({}, {
+            useMasterKey: true
+        });
     }
+
+    const rquery = new Parse.Query(Parse.Role);
+    rquery.equalTo("name", "employee");
+    const employeeRole = await rquery.first({
+        useMasterKey: true
+    });
+    employeeRole.relation("users").add(req.object);
+    await employeeRole.save({}, {
+        useMasterKey: true
+    })
 })
