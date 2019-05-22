6f5ca531ffdd3ddb5d2bd7a347110990a7b5ce6a
RudsonA <rudson.augustin@yahoo.com>
2019-05-22 11:16:48 -0400

    restructuring

--- cloud/main.js
+++ cloud/main.js
@@ -2,13 +2,12 @@
  * Place Parse Cloud Code Here
  */
 
-require('./beforeSave');
-require('./afterSave');
+require('./save');
 
-Parse.Cloud.define('test_push_services', function( req, res) {
+Parse.Cloud.define('test_push_services', function (req, res) {
     Parse.Push.send({
         data: {
-            alert:"This is a test from Parse Server"
+            alert: "This is a test from Parse Server"
         }
     });
 });
