789ead796718aa44acd81440f8cf069623ca2703
RudsonA <rudson.augustin@yahoo.com>
2019-06-03 10:47:42 -0400

    files being saved, finally.

--- cloud/saves/Files.js
+++ cloud/saves/Files.js
@@ -12,8 +12,13 @@ Parse.Cloud.afterSave('File', async req => {
         // spawn new parse object
         let newLibraryItem = new Parse.Object('Library2');
         let item = objects[i];
+        newLibraryItem.set('file', {
+            __type: "Pointer",
+            className: "File",
+            objectId: req.object.id
+        });
         Object.keys(item).forEach(key => {
-            newLibraryItem.set(key, items[key]);
+            newLibraryItem.set(key, item[key]);
         });
         try {
             await newLibraryItem.save({}, {
@@ -25,68 +30,57 @@ Parse.Cloud.afterSave('File', async req => {
         }
 
     }
-    // STill not hitting it
-    // query.equalTo("file", req.object);
-    // let u = await query.first({
-    //     useMasterKey: true
-    // });
+})
 
+/*
+    var query = new Parse.Query('Library');
 
-
-    // I just did, should I start parse on browser to upload a file?
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // const parseFile = new Parse.File(name, file); // that's what sdk documentation say
+    query.equalTo("file", req.object);
+    let u = await query.first({
+        useMasterKey: true
+    });
 
     // Putting csv to json object. 
-    // let file = fs.readFileSync('../csv/sample.csv'); // request object attribute
-    // let parsed = await parser.parseCSVBuffer(file);
-    // let jsoncsv = parser.parsedToObjects(parsed);
+    /*  // let file = fs.readFileSync('../csv/sample.csv'); // request object attribute
+      let file = fs.readFileSync(dataBuffer);
+      let parsed = await parser.parseCSVBuffer(file);
+      let jsoncsv = parser.parsedToObjects(parsed);
+      console.log(jsoncsv); ***
 
-    // var arr = [];
+    var arr = [];
 
     // json object to Parse library 
-    // if (!u) {
-    //     var library = new Parse.Object('Library2');
+    if (!u) {
+        var library = new Parse.Object('Library2');
 
-    //     jsoncsv.forEach((x) => {
-    //         let row = {
-    //             title: x.title,
-    //             url: x.url,
-    //             KAT: x.ka,
-    //             competency: x.cl,
-    //             type: x.type,
-    //             file: {
-    //                 __type: "Pointer",
-    //                 className: "File",
-    //                 objectId: req.object.id
-    //             }
-    //         }
-    //         arr.push(row);
-    //     })
+        // Objects replacing jsoncsv 
+        objects.forEach((x) => {
+            let row = {
+                title: x.title,
+                url: x.url,
+                KAT: x.ka,
+                competency: x.cl,
+                type: x.type,
+                file: {
+                    __type: "Pointer",
+                    className: "File",
+                    objectId: req.object.id
+                }
+            }
+            arr.push(row);
+        })
 
-    //     console.log(arr);
+        console.log(arr);
 
-    //     for (let i = 0; i < arr.length; i++) {
-    //         const element = arr[i];
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
 
-    //         const xhttp = new XMLHttpRequest();
-    //         xhttp.open("POST", "http://localhost:1337/parse/classes/Library2", true);
-    //         xhttp.setRequestHeader("X-Parse-Application-id", "your_app_id");
-    //         xhttp.setRequestHeader("X-Parse-REST-API-Key", "client_key");
-    //         xhttp.setRequestHeader("Content-Type", "application/json");
-    //         xhttp.send(JSON.stringify(element));
-    //     }
-    // }
-})
+            const xhttp = new XMLHttpRequest();
+            xhttp.open("POST", "http://localhost:1337/parse/classes/Library2", true);
+            xhttp.setRequestHeader("X-Parse-Application-id", "your_app_id");
+            xhttp.setRequestHeader("X-Parse-REST-API-Key", "client_key");
+            xhttp.setRequestHeader("Content-Type", "application/json");
+            xhttp.send(JSON.stringify(element));
+        }
+    }
+    */
